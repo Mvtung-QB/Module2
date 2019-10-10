3098af98c27234e7ab35bc1017d3f33b2f96850a
Mvtung-QB <shane92mai@gmail.com>
2019-10-10 11:36:52 +0700

    up demo

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,20 +1,19 @@
 package com.company;
 
 import javax.swing.*;
-import java.lang.reflect.Array;
+import java.lang.*;
+import java.util.Scanner;
 
-public static class Main {
-    static int a = 1, b = 2;
-    static int c;
-    static int arr[] = new int[10];
-    static final String letter = "á";
+public  class Main {
+     int a = 1, b = 2;
+
+     int arr[] = new int[10];
+     final String letter = "á";
     Scanner sc = new Scanner(System.in);
-    c = sc.nextInt();
-    for(int i=0; i< arr.length ; i++){
+    int c = sc.nextInt();
 
-    }
     public static void main(String[] args) {
         // write your code here
-        System.out.println(a + b + letter);
+        System.out.println(c);
     }
 }
