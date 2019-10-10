82ff8f15eded17fa34e202186600075eac45cf97
Mvtung-QB <shane92mai@gmail.com>
2019-10-10 11:27:51 +0700

    demo

---
+++ src/com/company/Main.java
@@ -0,0 +1,20 @@
+package com.company;
+
+import javax.swing.*;
+import java.lang.reflect.Array;
+
+public static class Main {
+    static int a = 1, b = 2;
+    static int c;
+    static int arr[] = new int[10];
+    static final String letter = "á";
+    Scanner sc = new Scanner(System.in);
+    c = sc.nextInt();
+    for(int i=0; i< arr.length ; i++){
+
+    }
+    public static void main(String[] args) {
+        // write your code here
+        System.out.println(a + b + letter);
+    }
+}
